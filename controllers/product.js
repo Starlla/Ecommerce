34fe9b771d60b9a71dc21d8700456e28e749fe0c
Claire <claire.cy.t@gmail.com>
2021-01-29 14:53:50 -0500

    List related products

--- controllers/product.js
+++ controllers/product.js
@@ -146,6 +146,24 @@ exports.list = (req, res) => {
     res.json(data)
   })
 }
+/*
+* it will find the products based on the req product category
+* other products taht has the same category, will be returned
+*/
+exports.listRelated = (req, res) => {
+  let limit = req.query.limit? parseInt(req.query.limit): 6
+
+  Product.find({_id:{$ne: req.product}, category: req.product.category}).limit(limit)
+  .populate('category', '_id name')
+  .exec((err, products)=>{
+    if(err){
+      return res.status(400).json({
+        err: 'Products not found'
+      }) 
+    }
+    res.json(products);
+  })
+}
 
 
 
