54f4623fed4197fe982f2028c55144f1f156d7b2
Claire <claire.cy.t@gmail.com>
2021-02-01 18:02:26 -0500

    Add profile read and update route

--- controllers/user.js
+++ controllers/user.js
@@ -12,3 +12,26 @@ exports.userById = (req, res, next, id) => {
     next();
   });
 }
+
+exports.read = (req, res) => {
+  req.profile.hashed_password = undefined
+  req.profile.salt = undefined
+  return res.json(req.profile);
+}
+
+exports.update = (req, res) => {
+  User.findOneAndUpdate({
+    _id: req.profile.id
+  }, {$set: req.body}, {new: true}, (err, user)=>{
+    if(err){
+      return res.status(400).json({
+        error: 'You are not authorized to perform this action'
+      })
+    }
+    console.log(user);
+    user.hashed_password = undefined
+    user.salt = undefined;
+    res.json(user);
+
+  })
+}
